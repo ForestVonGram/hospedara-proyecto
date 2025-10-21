2eaac404eeb4041dfe0a6ecfd1bb21bc60f0b758
David Gómez Ramírez <forest@MacBook-Air-de-David.local>
2025-10-21 15:16:34 -0500

    Correción error en crearNotificacion.

--- backend/src/main/java/com/hospedaya/backend/application/mapper/NotificacionMapper.java
+++ backend/src/main/java/com/hospedaya/backend/application/mapper/NotificacionMapper.java
@@ -4,6 +4,7 @@ import com.hospedaya.backend.application.dto.notificacion.NotificacionRequestDTO
 import com.hospedaya.backend.application.dto.notificacion.NotificacionResponseDTO;
 import com.hospedaya.backend.domain.entity.Notificacion;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -11,7 +12,9 @@ public interface NotificacionMapper {
 
     NotificacionMapper INSTANCE = Mappers.getMapper(NotificacionMapper.class);
 
+    @Mapping(target = "usuario.id", source = "usuarioId")
     Notificacion toEntity(NotificacionRequestDTO dto);
 
+    @Mapping(target = "usuarioId", source = "usuario.id")
     NotificacionResponseDTO toResponse(Notificacion entity);
 }
